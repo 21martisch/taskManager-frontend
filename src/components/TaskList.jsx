3050b7db850ff604e36cb08d2fd68146c68265af
21martisch <martinaschaller12@gmail.com>
2024-12-30 00:55:46 -0300

    update list

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -15,10 +15,22 @@ const TaskList = () => {
   );
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-6">
-      {sortedTasks.map((task) => (
-        <TaskItem key={task._id} task={task} />
-      ))}
+    <div className="mt-6">
+      {sortedTasks.length === 0 ? (
+        <p className="text-gray-700 text-center">
+          {filter === "completed"
+            ? "No hay tareas completadas."
+            : filter === "pending"
+            ? "No hay tareas pendientes."
+            : "No hay tareas disponibles."}
+        </p>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          {sortedTasks.map((task) => (
+            <TaskItem key={task._id} task={task} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
